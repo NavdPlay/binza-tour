d8bffbda61c9b7709f256a8c8119c7ab38ea7115
Naadir Al Atilla Muklis <naadiralatilla14@gmail.com>
2026-01-27 13:33:11 +0200

    add more packages

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const hamburger = document.getElementById("hamburger");
+const wrapper = document.querySelector('.card-wrapper');
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
 });
 
 document.getElementById('year').innerHTML = new Date().getFullYear();
+
+
+document.getElementById('next').onclick = () => {
+  wrapper.scrollBy({ left: 340, behavior: 'smooth' });
+};
+
+document.getElementById('prev').onclick = () => {
+  wrapper.scrollBy({ left: -340, behavior: 'smooth' });
+};
